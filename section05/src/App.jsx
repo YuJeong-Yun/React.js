5ea5785ebeca39865f8b97cf8f9f02d1c45d6f2e
YYJ <yunyj99@naver.com>
2024-04-29 16:14:45 +0900

    5-8) State로 사용자 입력관리하기
    input, select, textarea

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,17 +1,10 @@
-// 컴포넌트 리렌더링 되는 3가지 경우
-// 1. 자신이 관리하는 state의 값이 변경되었을 때
-// 2. 자신이 제공받는 props의 값이 변경되었을 때
-// 3. 부모 컴포넌트가 리렌더링 되면 자식 컴포넌트도 리렌더링 됨
-
 import "./App.css";
-import Bulb from "./components/Bulb";
-import Counter from "./components/Counter";
+import Register from "./components/Register";
 
 function App() {
   return (
     <>
-      <Bulb />
-      <Counter />
+      <Register />
     </>
   );
 }
