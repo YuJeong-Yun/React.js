91405ea2869d424ed3ea41773e41bfa0882f611d
YYJ <yunyj99@naver.com>
2024-04-30 09:56:40 +0900

    7-1) 라이프사이클
    Mount, Update, UnMount

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,3 +1,14 @@
+// 리액트 컴포넌트의 라이프사이클
+// 1. Mount
+//  - 컴포넌트가 탄생하는 순간 = 화면에 렌더링 되는 순간 :: 서버에서 데이터를 불러오는 작업
+// 2. Update
+//  - 컴포넌트가 다시 렌더링되는 순간 = 리렌더링 될 때 :: 변경 값 콘솔에 출력
+// 3. UnMount
+//  - 컴포넌트가 화면에서 사라지는 순간 = 렌더링에서 제외되는 순간 :: 컴포넌트가 사용하던 메모리 정리
+
+// useEffect
+// 리액트 컴포넌트의 사이드 이펙트를 제어하는 새로운 React Hook
+
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
