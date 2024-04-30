8fe79f62eecc32869b9531d0f4a447ad2a6661c1
YYJ <yunyj99@naver.com>
2024-04-30 10:25:41 +0900

    7-2) useEffect 사용하기

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,21 +1,17 @@
-// 리액트 컴포넌트의 라이프사이클
-// 1. Mount
-//  - 컴포넌트가 탄생하는 순간 = 화면에 렌더링 되는 순간 :: 서버에서 데이터를 불러오는 작업
-// 2. Update
-//  - 컴포넌트가 다시 렌더링되는 순간 = 리렌더링 될 때 :: 변경 값 콘솔에 출력
-// 3. UnMount
-//  - 컴포넌트가 화면에서 사라지는 순간 = 렌더링에서 제외되는 순간 :: 컴포넌트가 사용하던 메모리 정리
-
-// useEffect
-// 리액트 컴포넌트의 사이드 이펙트를 제어하는 새로운 React Hook
-
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  // 두 번째 인수로 전달한 배열(=의존성 배열=dependency array=deps)에 들어 있는 값이 바뀌면
+  // SideEffect로서 첫 번째 인수로 전달한 콜백 함수를 실행시켜 줌
+  useEffect(() => {
+    console.log(count);
+  }, [count, input]); // useState 값이 변화할 때 마다 출력됨
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -24,6 +20,14 @@ function App() {
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+      </section>
       <section>
         <Viewer count={count} />
       </section>
