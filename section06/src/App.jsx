5ba50c19b488f51f50915b8967150ba2ab1b188b
YYJ <yunyj99@naver.com>
2024-04-30 09:09:52 +0900

    6-2) UI 구현하기

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
 
 function App() {
-  return <>카운터 엡</>;
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+
+      <section>
+        <Controller />
+      </section>
+    </div>
+  );
 }
 
 export default App;
