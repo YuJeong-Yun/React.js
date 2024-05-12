3326ece507a5de9c492766f1d2c2d818bec042e0
YUJEONG YUN <yunyj99@naver.com>
2024-05-12 19:04:07 +0900

    10-2) useMemo와 연산 최적화

--- section10/src/components/List.jsx
+++ section10/src/components/List.jsx
@@ -1,6 +1,6 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 function List({ todos, onUpdate, onDelete }) {
   const [search, setSearch] = useState("");
@@ -20,9 +20,31 @@ function List({ todos, onUpdate, onDelete }) {
 
   const filteredTodos = getFilteredData();
 
+  // const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
+  // 의존성 배열 : deps (useEffect에도 사용되었음)
+  // deps에 포함된 값이 변경되었을 때에만 첫 번째 인수로 전달한 콜백 함수를 다시 실행
+  // => 콜백 함수 안에 메모이제이션 하고 싶은 연산 넣어주면 됨
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+
   return (
     <div className="List">
       <h4>Todo List ✔</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch}
