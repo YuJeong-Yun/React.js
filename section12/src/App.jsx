81465da283409d4f8d22496f01c8726fa9a360a7
YYJ <yunyj99@naver.com>
2024-05-14 10:46:45 +0900

    12-8) 일기 관리 기능 구현하기1

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,56 +1,40 @@
-// 이미지는 assets 폴더 아래에 -> 브라우저가 캐싱해서 이미지 최적화 가능
-// but 이미지가 많을 때에는 캐싱 용량이 많아지므로 public 폴더 아래에 두는게 나을 수도 있음
-
 import "./App.css";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { useReducer } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import Edit from "./pages/Edit";
 import NotFount from "./pages/NotFound";
-import Button from "./components/Button";
-import Header from "./components/Header";
 
-import { getEmotionImage } from "./util/get-emotion-image";
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
 
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  };
-
+  const [data, dispatch] = useReducer(reducer, mockData);
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"left"} />}
-        rightChild={<Button text={"Right"} />}
-      />
-      <Button
-        text={"123"}
-        type={"DEFAULT"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
-      <Button
-        text={"123"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
-      <Button
-        text={"123"}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/new" element={<New />}></Route>
         <Route path="/diary/:id" element={<Diary />}></Route>
+        <Route path="/edit/:id" element={<Edit />}></Route>
         <Route path="*" element={<NotFount />} />
       </Routes>
     </>
